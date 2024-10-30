c371d8e28b53d613b1c3143a1f777185b16c47d1
zeck00 <a2005sdcardemom@gmail.com>
2024-10-30 17:39:30 +0400

    everything is working fine, manager, generator combination is wonderfully working. home is updated with a scrollable widget.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -29,205 +29,207 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: propWidth(16),
-            vertical: propHeight(10),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Top bar with logo and profile icon
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SvgPicture.asset(
-                    'assets/logo.svg',
-                    height: propHeight(55),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      // Handle profile tap
-                    },
-                    child: CircleAvatar(
-                      radius: propWidth(22.5),
-                      backgroundColor: AppColors.black,
-                      child: SvgPicture.asset('assets/profile.svg'),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: propWidth(16),
+              vertical: propHeight(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Top bar with logo and profile icon
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/logo.svg',
+                      height: propHeight(55),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Handle profile tap
+                      },
+                      child: CircleAvatar(
+                        radius: propWidth(22.5),
+                        backgroundColor: AppColors.black,
+                        child: SvgPicture.asset('assets/profile.svg'),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: propHeight(20)),
+
+                // MiraiDropdownWidget for Course Selection
+                Text(
+                  "Select a Course",
+                  style: FontStyles.hometitle,
+                ),
+                SizedBox(height: propHeight(10)),
+
+                MiraiDropDownMenu<String>(
+                  valueNotifier: _selectedCourseNotifier,
+                  itemWidgetBuilder: (int index, String? item,
+                      {bool isItemSelected = false}) {
+                    return MiraiDropDownItemWidget(
+                      item: item ?? '',
+                      isItemSelected: isItemSelected,
+                    );
+                  },
+                  children: _courses,
+                  onChanged: (String newValue) {
+                    // if (newValue == 'Health Awareness and Nutrition') {
+                    //   Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //       builder: (context) => ChapterSelectionScreen(),
+                    //     ),
+                    //   );
+                    // }
+                    // _selectedCourseNotifier.value = newValue;
+                  },
+                  showSeparator: true,
+                  showMode: MiraiShowMode.bottom,
+                  maxHeight: propHeight(150),
+                  child: Container(
+                    key: GlobalKey(), // Add this key
+                    padding: EdgeInsets.symmetric(horizontal: propWidth(15)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(propHeight(15)),
+                      border: Border.all(
+                          color: AppColors.black, width: propHeight(1.5)),
+                    ),
+                    height: propHeight(50),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        ValueListenableBuilder<String>(
+                          valueListenable: _selectedCourseNotifier,
+                          builder: (_, String chosenTitle, __) {
+                            return Text(
+                              chosenTitle,
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                              overflow: TextOverflow.ellipsis,
+                            );
+                          },
+                        ),
+                        Icon(
+                          Icons.arrow_drop_down_rounded,
+                          size: propHeight(40),
+                          color: AppColors.black,
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: propHeight(20)),
+                ),
 
-              // MiraiDropdownWidget for Course Selection
-              Text(
-                "Select a Course",
-                style: FontStyles.hometitle,
-              ),
-              SizedBox(height: propHeight(10)),
+                SizedBox(height: propHeight(15)),
 
-              MiraiDropDownMenu<String>(
-                valueNotifier: _selectedCourseNotifier,
-                itemWidgetBuilder: (int index, String? item,
-                    {bool isItemSelected = false}) {
-                  return MiraiDropDownItemWidget(
-                    item: item ?? '',
-                    isItemSelected: isItemSelected,
-                  );
-                },
-                children: _courses,
-                onChanged: (String newValue) {
-                  // if (newValue == 'Health Awareness and Nutrition') {
-                  //   Navigator.push(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //       builder: (context) => ChapterSelectionScreen(),
-                  //     ),
-                  //   );
-                  // }
-                  // _selectedCourseNotifier.value = newValue;
-                },
-                showSeparator: true,
-                showMode: MiraiShowMode.bottom,
-                maxHeight: propHeight(150),
-                child: Container(
-                  key: GlobalKey(), // Add this key
-                  padding: EdgeInsets.symmetric(horizontal: propWidth(15)),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(propHeight(15)),
-                    border: Border.all(
-                        color: AppColors.black, width: propHeight(1.5)),
-                  ),
-                  height: propHeight(50),
+                // EduMentor AI Section
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChapterSelectionScreen(),
+                      ),
+                    );
+                  },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      ValueListenableBuilder<String>(
-                        valueListenable: _selectedCourseNotifier,
-                        builder: (_, String chosenTitle, __) {
-                          return Text(
-                            chosenTitle,
-                            style: TextStyle(fontWeight: FontWeight.w600),
-                            overflow: TextOverflow.ellipsis,
-                          );
-                        },
-                      ),
-                      Icon(
-                        Icons.arrow_drop_down_rounded,
-                        size: propHeight(40),
-                        color: AppColors.black,
+                    children: [
+                      Text("EduMentor AI", style: FontStyles.hometitle),
+                      SvgPicture.asset(
+                        'assets/g-next.svg',
+                        height: propHeight(30),
                       ),
                     ],
                   ),
                 ),
-              ),
+                SizedBox(height: propHeight(10)),
 
-              SizedBox(height: propHeight(15)),
+                // EduMentor AI Card
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChapterSelectionScreen(),
+                      ),
+                    );
+                  },
+                  child: _customCard('assets/EduMentor.png'),
+                ),
+                SizedBox(height: propHeight(20)),
 
-              // EduMentor AI Section
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChapterSelectionScreen(),
-                    ),
-                  );
-                },
-                child: Row(
+                // Course Material Section
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CourseMaterialPage(),
+                      ),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Course Material", style: FontStyles.hometitle),
+                      SvgPicture.asset(
+                        'assets/b-next.svg',
+                        height: propHeight(30),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: propHeight(10)),
+
+                // Course Material Card
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CourseMaterialPage(),
+                      ),
+                    );
+                  },
+                  child: _customCard('assets/CourseMaterial.png'),
+                ),
+                SizedBox(height: propHeight(30)),
+
+                // Bottom Buttons (Settings & About Us)
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("EduMentor AI", style: FontStyles.hometitle),
-                    SvgPicture.asset(
-                      'assets/g-next.svg',
-                      height: propHeight(30),
+                    _bottomButton(
+                      "Settings",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AboutUsPage(),
+                          ),
+                        );
+                      },
+                    ),
+                    _bottomButton(
+                      "About Us",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AboutUsPage(),
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: propHeight(10)),
-
-              // EduMentor AI Card
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChapterSelectionScreen(),
-                    ),
-                  );
-                },
-                child: _customCard('assets/EduMentor.png'),
-              ),
-              SizedBox(height: propHeight(20)),
-
-              // Course Material Section
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CourseMaterialPage(),
-                    ),
-                  );
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Course Material", style: FontStyles.hometitle),
-                    SvgPicture.asset(
-                      'assets/b-next.svg',
-                      height: propHeight(30),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: propHeight(10)),
-
-              // Course Material Card
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CourseMaterialPage(),
-                    ),
-                  );
-                },
-                child: _customCard('assets/CourseMaterial.png'),
-              ),
-              SizedBox(height: propHeight(30)),
-
-              // Bottom Buttons (Settings & About Us)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _bottomButton(
-                    "Settings",
-                    () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AboutUsPage(),
-                        ),
-                      );
-                    },
-                  ),
-                  _bottomButton(
-                    "About Us",
-                    () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AboutUsPage(),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
