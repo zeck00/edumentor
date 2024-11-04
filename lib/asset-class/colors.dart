3c1b4b20c17ec1ca633b0aa24ae9d73186037be7
zeck00 <a2005sdcardemom@gmail.com>
2024-11-04 23:49:18 +0400

    color addition

--- lib/asset-class/colors.dart
+++ lib/asset-class/colors.dart
@@ -5,4 +5,9 @@ class AppColors {
   static const Color black = Color(0xFF2F2F2F); // Black
   static const Color white = Color(0xFFF3F3F3); // White
   static const Color gray = Color.fromARGB(215, 215, 215, 215); // Gray
+  static const Color darkGray = Color(0xFFA9A9A9); // Dark Gray
+  static const Color lightGray = Color(0xFFD3D3D3); // Light Gray
+  static const Color blue = Color(0xFF007ACC); // Blue
+  static const Color red = Color(0xFFFF0000); // Red
+  static const Color yellow = Color(0xFFFF00); // Yellow
 }
